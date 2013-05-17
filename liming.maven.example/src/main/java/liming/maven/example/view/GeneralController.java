ad95bca33c130d87e2d6de7a0bc88b9c3ad6cdae
bluesroot <bluesroot@gmail.com>
2013-05-17 16:52:46 +0800

    修改注释

--- liming.maven.example/src/main/java/liming/maven/example/view/GeneralController.java
+++ liming.maven.example/src/main/java/liming/maven/example/view/GeneralController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 /**
- * git提交测试
+ * git提交测试----
  * @author phantomes
  *
  */
