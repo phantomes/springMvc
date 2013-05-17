d62de946ae0dcffc871d90ae4fdaead9bd68127a
bluesroot <bluesroot@gmail.com>
2013-05-17 16:30:04 +0800

    首次commit测试

---
+++ liming.maven.example/src/main/java/liming/maven/example/view/GeneralController.java
@@ -0,0 +1,15 @@
+package liming.maven.example.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GeneralController {
+
+	@RequestMapping(value="index.html")
+	public void index_jsp(Model model){
+		model.addAttribute("liming", "黎明你好");
+		System.out.println("index.jsp");
+	}
+}
